a4f3acaefe78e5f2bfe3dad4a8197bd469e0a331
aa-wong <waaron.wong@gmail.com>
2021-05-29 22:00:42 -0400

    modifications to IPFS

--- dist/Block3.js
+++ dist/Block3.js
@@ -98882,17 +98882,19 @@ __webpack_require__.r(__webpack_exports__);
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-const {
-  NFTStorage,
-  Blob
-} = __webpack_require__(/*! nft.storage */ "./node_modules/nft.storage/src/lib.js");
+/* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./utils */ "./src/utils/index.js");
+/* harmony import */ var nft_storage__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! nft.storage */ "./node_modules/nft.storage/src/lib.js");
+
+
 
 class IPFSStorageManager {
   constructor(apiKey) {
     if (apiKey) {
-      this.client = new NFTStorage({
+      this.client = new nft_storage__WEBPACK_IMPORTED_MODULE_1__["NFTStorage"]({
         token: apiKey
       });
+      this.http = new _utils__WEBPACK_IMPORTED_MODULE_0__["Http"]();
+      this.http.host = 'https://ipfs.io/ipfs/';
     } else throw new Error('NFTStorage apiKey must be provided');
   }
 
@@ -98901,8 +98903,12 @@ class IPFSStorageManager {
   }
 
   storeDataBlob(metadata) {
-    const content = new Blob([metadata]);
+    const content = new nft_storage__WEBPACK_IMPORTED_MODULE_1__["Blob"]([metadata]);
     return this.client.storeBlob(content);
+  }
+
+  delete(cid) {
+    return this.client.delete(cid);
   }
 
   createNFTMetaDataTemplate(description, externalUrl, name, image, attributes) {
@@ -98923,7 +98929,28 @@ class IPFSStorageManager {
         const meta = this.createNFTMetaDataTemplate(description, externalUrl, name, `https://ipfs.io/ipfs/${fileCid}/${file.name}`, attributes);
         const metaCid = await this.storeDataBlob(meta);
         if (cb) cb(1);
-        return resolve(`https://ipfs.io/ipfs/${metaCid}`);
+        return resolve(metaCid);
+      } catch (e) {
+        return reject(e);
+      }
+    });
+  }
+
+  deleteMetaData(cid, cb) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const {
+          image
+        } = await this.http.get(cid);
+
+        if (image) {
+          await this.delete(image.split('/')[4]);
+          if (cb) cb(0.5);
+        }
+
+        await this.delete(cid);
+        if (cb) cb(1);
+        return resolve();
       } catch (e) {
         return reject(e);
       }
--- src/IPFS.js
+++ src/IPFS.js
@@ -1,12 +1,15 @@
-const {
+import { Http } from './utils';
+import {
   NFTStorage,
   Blob
-} = require('nft.storage');
+} from'nft.storage';
 
 class IPFSStorageManager {
   constructor(apiKey) {
     if (apiKey) {
       this.client = new NFTStorage({ token: apiKey });
+      this.http = new Http();
+      this.http.host = 'https://ipfs.io/ipfs/';
     } else throw new Error('NFTStorage apiKey must be provided');
   }
 
@@ -17,6 +20,10 @@ class IPFSStorageManager {
   storeDataBlob(metadata) {
     const content = new Blob([metadata]);
     return this.client.storeBlob(content);
+  }
+
+  delete(cid) {
+    return this.client.delete(cid);
   }
 
   createNFTMetaDataTemplate(description, externalUrl, name, image, attributes) {
@@ -43,6 +50,25 @@ class IPFSStorageManager {
         const metaCid = await this.storeDataBlob(meta);
         if (cb) cb(1);
         return resolve(metaCid);
+      } catch (e) {
+        return reject(e);
+      }
+    });
+  }
+
+  deleteMetaData(cid, cb) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const { image } = await this.http.get(cid);
+
+        if (image) {
+          await this.delete(image.split('/')[4]);
+          if (cb) cb(0.5);
+        }
+
+        await this.delete(cid);
+        if (cb) cb(1);
+        return resolve();
       } catch (e) {
         return reject(e);
       }
